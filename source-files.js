ab365c3fbb79d7e42934f7fbf3bd797da4a46023
artichoke-ci <ci@artichokeruby.org>
2020-06-14 13:24:40 +0000

    deploy: 0bf7a10c602eab596505283d6623398f3ee2b71e

--- source-files.js
+++ source-files.js
@@ -1,5 +1,9 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["bstr"] = {"name":"","dirs":[{"name":"byteset","files":["mod.rs","scalar.rs"]},{"name":"search","files":["byte_frequencies.rs","mod.rs","prefilter.rs","twoway.rs"]}],"files":["ascii.rs","bstr.rs","cow.rs","ext_slice.rs","impls.rs","lib.rs","utf8.rs"]};
+sourcesIndex["bstr"] = {"name":"","dirs":[{"name":"byteset","files":["mod.rs","scalar.rs"]},{"name":"search","files":["byte_frequencies.rs","mod.rs","prefilter.rs","twoway.rs"]},{"name":"unicode","dirs":[{"name":"fsm","files":["grapheme_break_fwd.rs","grapheme_break_rev.rs","mod.rs","regional_indicator_rev.rs","sentence_break_fwd.rs","simple_word_fwd.rs","whitespace_anchored_fwd.rs","whitespace_anchored_rev.rs","word_break_fwd.rs"]}],"files":["grapheme.rs","mod.rs","sentence.rs","whitespace.rs","word.rs"]}],"files":["ascii.rs","bstr.rs","bstring.rs","cow.rs","ext_slice.rs","ext_vec.rs","impls.rs","io.rs","lib.rs","utf8.rs"]};
+sourcesIndex["byteorder"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["intaglio"] = {"name":"","files":["lib.rs","str.rs"]};
+sourcesIndex["lazy_static"] = {"name":"","files":["inline_lazy.rs","lib.rs"]};
 sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
+sourcesIndex["regex_automata"] = {"name":"","files":["classes.rs","dense.rs","dfa.rs","lib.rs","regex.rs","sparse.rs","state_id.rs"]};
+sourcesIndex["serde"] = {"name":"","dirs":[{"name":"de","files":["from_primitive.rs","ignored_any.rs","impls.rs","mod.rs","utf8.rs","value.rs"]},{"name":"private","files":["de.rs","macros.rs","mod.rs","ser.rs"]},{"name":"ser","files":["fmt.rs","impls.rs","impossible.rs","mod.rs"]}],"files":["export.rs","integer128.rs","lib.rs","macros.rs"]};
 createSourceSidebar();
